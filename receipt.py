f6def50746f93fe2d27055cec56e79340831bcb1
Sujay Koujalgi <suk96126@gmail.com>
2023-10-03 21:48:15 -0500

    added custom test cases

--- receipt.py
+++ receipt.py
@@ -6,10 +6,52 @@ import uuid
 import yaml
 import jsonschema
 from jsonschema import validate
+import datetime
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
+def boundry_check(data):
+    #Check 1: Check date format
+    date_pattern = r"^\d{4}-\d{2}-\d{2}$"
+    if not re.match(date_pattern, data['purchaseDate']):
+        return False, "date check failed"
+    
+    #Check 2: Check date range
+    try:
+        date_format = '%Y-%m-%d'
+        date_object = datetime.datetime.strptime(data['purchaseDate'], date_format)
+    except ValueError:
+        return False, "date range check failed"
+    
+    #Check 3: Check Time Format and range
+    time_pattern = r"^\d{2}:\d{2}$"
+    if not re.match(time_pattern, data['purchaseTime']):
+        return False, "time regex check failed"
+    time_value = data['purchaseTime'].split(":")
+    if not (0<=int(time_value[0])<24 and 0<=int(time_value[1])<60):
+        return False, "time range check failed"
+    
+    #Check 3: Check min items
+    # Done by yml validator
+
+    #Check 4: regex for description
+    # Done by yml validator
+
+    #Check 5: negative prices and negative total
+    # Done by yml regex validator
+
+    #Check 6: Check total equals sum of prices
+    tmp = 0.00
+    for item in data["items"]:
+        tmp += round(float(item["price"]),2)
+    if round(tmp,2) != round(float(data["total"]),2):
+        return False, "Sum of prices doesn't match total"
+
+
+    else:
+        return True, "all cases passed"
 # Load the JSON schema from the YAML file
 with open("receipt_api.yml", "r") as schema_file:
     schema = yaml.safe_load(schema_file)
@@ -31,6 +73,9 @@ def generate_id():
         # Validate the request data against the loaded schema
         validate(instance=data, schema=schema)
 
+        boolean, msg = boundry_check(data)
+        if not boolean:   
+            return jsonify({"msg": msg}), 400
         # If validation succeeds, process the request
         # ...
         receipt_id = uuid.uuid4()
